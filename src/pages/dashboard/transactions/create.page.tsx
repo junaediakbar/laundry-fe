bbfab24b2ac737ea50e2bfb430c742c44cdf6dee
Junaedi Akbar <68473577+junaediakbar@users.noreply.github.com>
2025-03-11 21:14:24 +0700

    Update create.page.tsx

--- src/pages/dashboard/transactions/create.page.tsx
+++ src/pages/dashboard/transactions/create.page.tsx
@@ -1,4 +1,4 @@
-/import moment from 'moment';
+import moment from 'moment';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
